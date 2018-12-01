e7d8e4619f91d49a07e7fe2b0cb0d8f12ccd9ace
Brett Anthony <nwpbusiness@gmail.com>
2018-11-30 23:54:30 -0700

    Quick fix on package batch script.

--- package_files_for_release.bat
+++ package_files_for_release.bat
@@ -6,14 +6,10 @@ CLS
 
 SET "sevzip="C:\Program Files\7-Zip\7z.exe""
 SET "version=1.0.1.0"
-SET "filelist=HLAEServer.dll Release_x86 Release_x64 README.txt"
+SET "filelist=Release_x86 Release_x64 README.txt"
 
-CD release_x86
 %sevzip% a -t7z HLAEServer-%version%.7z %filelist%
 %sevzip% a -tzip HLAEServer-%version%.zip %filelist%
-
-MOVE HLAEServer-%version%.7z ..\
-MOVE HLAEServer-%version%.zip ..\
 
 ECHO.
 ECHO Packaged.
